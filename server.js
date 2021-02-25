290cd774c555530662521260d451516e3409886e
SonBui <lap14008@LAP14008s-MacBook-Pro.local>
2021-02-25 15:20:34 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+app.use(express.static('staticLive'));
+
+app.listen(PORT, () => {
+    console.log('Server started on port ' + PORT);
+});
