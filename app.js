e61cc19bbff91f0409a246957b8b44f6eb14f053
SonBui <lap14008@LAP14008s-MacBook-Pro.local>
2021-02-25 17:33:28 +0700

    make file auto download

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
+// const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/*', (req, res) => {
+    console.log(path.dirname(req.url), path.basename(req.url));
+    res.download('staticLive' + req.url);
 });
 
-app.use(express.static('staticLive'));
+// app.use(express.static('staticLive'));
 
 app.listen(port, () => {
     console.log('Server started on port ' + port);
