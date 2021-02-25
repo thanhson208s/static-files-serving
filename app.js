3506cc9b42127840b8f745c4639d92b176ceebd8
SonBui <lap14008@LAP14008s-MacBook-Pro.local>
2021-02-25 15:36:53 +0700

    add index

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.use(express.static('staticLive'));
 
